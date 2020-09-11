027392a7455f5be9cb35b9e78bdd83950efdd3d8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-11 13:50:30 +0100

    wip

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
+import { useFocus } from "./utils/useFocus";
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles";
 
 interface NewItemFormProps {
     onAdd(text: string): void;
 }
-export const NewItemForm = (props: NewItemFormProps) => {
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState("");
+    const inputRef = useFocus();
     return (
         <NewItemFormContainer>
             <NewItemInput
+                ref={inputRef}
                 value={text}
                 onChange={(e) => setText(e.target.value)}
             />
-            <NewItemButton onClick={() => text}>Create</NewItemButton>
+            <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
         </NewItemFormContainer>
     );
 };
