c659f401805b26e4e3b89b5f551e3025462e5fe0
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-12 20:09:16 +0100

    wip

--- src/Column.tsx
+++ src/Column.tsx
@@ -3,23 +3,34 @@ import { ColumnContainer, ColumnTitle } from "./styles";
 import { AddNewItem } from "./AddNewItem";
 import { useAppState } from "./AppStateContext";
 import { Card } from "./Card";
+import { useDrop } from "react-dnd";
 
 interface ColumnProps {
     text: String;
     index: number;
 }
 
-// export const Column = ({text}: ColumnProps) => {
-//     return (
-//         <ColumnContainer>
-//         <ColumnTitle>{text}</ColumnTitle>
-//         </ColumnContainer>
-//     )
-// }
+const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover(item: DragItem) {
+        const dragIndex = item.index;
+        const hoverIndex = index;
+        if (dragIndex === hoverIndex) {
+            return;
+        }
+        dispatch({ type: "MOVE_LIST", payload: { dragIndex, hoverIndex } });
+        item.index = hoverIndex;
+    },
+});
 export const Column = ({ text, index, id }: ColumnProps) => {
     const { state, dispatch } = useAppState();
+    const ref = useRef<HTMLDivElement>(null);
+
+    const { drag } = useItemDrag({ type: "COLUMN", id, index, text });
+
+    drag(drop(ref));
     return (
-        <ColumnContainer>
+        <ColumnContainer ref={ref}>
             <ColumnTitle>{text}</ColumnTitle>
             {state.lists[index].tasks.map((task, i) => (
                 <Card text={task.text} key={task.id} index={i} />
