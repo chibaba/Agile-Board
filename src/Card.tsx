815635b115be3406cfda5892140a1b7a5270f8dc
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-14 00:40:08 +0100

    wip

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 import { CardContainer } from "./styles";
+import { CardDragItem } from "./DragItem";
 
 interface CardProps {
     text: string;
 }
 
+const [, drop] = useDrop({
+    accept: "CARD",
+    hover(item: CardDragItem) {
+        if (item.id === id) {
+            return;
+        }
+        const dragIndex = item.index;
+        const hoverIndex = index;
+        const targetColumn = columnId;
+    },
+});
 export const Card = ({ text }: CardProps) => {
     return <CardContainer>{text}</CardContainer>;
 };
