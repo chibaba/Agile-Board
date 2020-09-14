c329a104711015d4882ce6776fa603b4726e870d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-14 14:45:53 +0100

    wip

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,30 +2,41 @@ import React from "react";
 
 import { CardContainer } from "./styles";
 import { CardDragItem } from "./DragItem";
+import { useAppState } from "./AppStateContext";
+import { useItemDrag } from "./useItemDrag";
 
 interface CardProps {
     text: string;
+    index: number;
+    id: string;
+    columnId: string;
+    isPreview?: boolean;
 }
 
-const [, drop] = useDrop({
-    accept: "CARD",
-    hover(item: CardDragItem) {
-        if (item.id === id) {
-            return;
-        }
-        const dragIndex = item.index;
-        const hoverIndex = index;
-        const sourceColumn = item.columnId;
-        const targetColumn = item.columnId;
+export const Card = ({ text, id, index, columnId, isPreview }: CardProps) => {
+    const { state, dispatch } = useAppState();
+    const ref = useRef<HTMLDivElement>(null);
+    const { drag } = useItemDrag({ type: "CARD", id, index, text, columnId });
+    const [, drop] = useDrop({
+        accept: "CARD",
+        hover(item: CardDragItem) {
+            if (item.id === id) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+            const sourceColumn = item.columnId;
+            const targetColumn = item.columnId;
 
-        dispatch({
-            type: "MOVE_TASK",
-            payload: { dragIndex, hoverIndex, sourceColumn, targetColumn },
-        });
-        item.index = hoverIndex;
-        item.columnId = targetColumn;
-    },
-});
-export const Card = ({ text }: CardProps) => {
-    return <CardContainer>{text}</CardContainer>;
+            dispatch({
+                type: "MOVE_TASK",
+                payload: { dragIndex, hoverIndex, sourceColumn, targetColumn },
+            });
+            item.index = hoverIndex;
+            item.columnId = targetColumn;
+        },
+    });
+    export const Card = ({ text }: CardProps) => {
+        return <CardContainer>{text}</CardContainer>;
+    };
 };
